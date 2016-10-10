75a7f59922bf915802dddd772d5da0248f111dbd
08manu <manuel.viso88@gmail.com>
2016-10-10 19:49:53 +0200

    Ahora si parte 3 acabada

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -8,23 +8,30 @@ class SmallSMILHandler(ContentHandler):
 
     def __init__ (self):
 
+        self.Diccionario = {'root-layout': ['width', 'height', 'backgroung-color'], 'region': ['id', 'bottom', 'left', 'right'], 'img':['src', 'region', 'begin', 'dur'], 'audio': ['src', 'begin', 'dur'], 'textstream': ['src', 'region']}
         self.Lista = []
 
-    def startElement(self, name, attrs):
+    def startElement(self, etiqueta, attrs):
+        dicc = {}
+        if etiqueta in self.Diccionario:
+            for atributo in self.Diccionario[etiqueta]:
+                dicc[atributo]= attrs.get(atributo, '')
+            self.Lista.append([etiqueta, dicc])
 
-        if name == 'root-layout':
-            self.root_layout = {'width': attrs.get('width', ""),
-                                'height': attrs.get('height',""),
-                                'background-color': attrs.get('background-color',"")}
-            self.Lista.append(self.root_layout)
 
-        elif name == 'region':
-            self.region = {'id': attrs.get('id', ""),
-                           'top': attrs.get('top',""),
-                           'bottom': attrs.get('bottom',""),
-                           'left': attrs.get('left',""),
-                           'right': attrs.get('right',"")}
-            self.Lista.append(self.region)
+        #if name == 'root-layout':
+            #self.root_layout = {'width': attrs.get('width', ""),
+                                #'height': attrs.get('height',""),
+                                #'background-color': attrs.get('background-color',"")}
+            #self.Lista.append(self.root_layout)
+
+        #elif name == 'region':
+            #self.region = {'id': attrs.get('id', ""),
+                           #'top': attrs.get('top',""),
+                           #'bottom': attrs.get('bottom',""),
+                           #'left': attrs.get('left',""),
+                           #'right': attrs.get('right',"")}
+            #self.Lista.append(self.region)
 
     def get_tags(self):
 
