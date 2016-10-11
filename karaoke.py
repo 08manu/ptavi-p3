d4959098de9089cea877677b28cdc36638ebc85f
08manu <manuel.viso88@gmail.com>
2016-10-11 13:33:27 +0200

    he intetado continuarla pero ha sido imposible

--- karaoke.py
+++ karaoke.py
@@ -2,23 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
+import urllib.request
 import smallsmilhandler
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-fich = sys.argv[-1]
+class KaraokeLocal():
 
-parser = make_parser()
-MyHandler = smallsmilhandler.SmallSMILHandler()
-parser.setContentHandler(MyHandler)
-parser.parse(open(fich))
-fichero = MyHandler.get_tags()
+    def __init__(self, fichero):
 
-if len(sys.argv) == 2:
-    for datos in fichero:
-        print(datos)
-else:
-    print("usage: python3 karaoke.py file.smil.")
+        parser = make_parser()
+        MyHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(MyHandler)
+        parser.parse(open(fichero))
+        self.lista = MyHandler.get_tags()
+
+    def __str__(self):
+        for etiqueta in fichero:
+            dicc_etiqueta = etiqueta[0]
+            for atributo in etiqueta:
+                dicc_atributo = atributo[0]
+
+    if len(sys.argv) == 2:
+        for datos in fichero:
+            print(datos)
+    else:
+        print("usage: python3 karaoke.py file.smil.")
 
 
