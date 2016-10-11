a3582161c237d63882c1f8c1ed00e8255fb43751
08manu <manuel.viso88@gmail.com>
2016-10-11 12:49:26 +0200

    Tenia otro pequeño error que ha sido solucionado

--- karaoke.py
+++ karaoke.py
@@ -7,20 +7,18 @@ import smallsmilhandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-#fich = sys.argv[-1]
+fich = sys.argv[-1]
 
-def imprimir(fich):
-    parser = make_parser()
-    MyHandler = smallsmilhandler.SmallSMILHandler()
-    parser.setContentHandler(MyHandler)
-    parser.parse(open(fich))
-    fichero = MyHandler.get_tags()
+parser = make_parser()
+MyHandler = smallsmilhandler.SmallSMILHandler()
+parser.setContentHandler(MyHandler)
+parser.parse(open(fich))
+fichero = MyHandler.get_tags()
 
-if __name__ == '__main__':
-
-    fich = sys.argv[-1]
-    print(imprimir(fich))
+if len(sys.argv) == 2:
     for datos in fichero:
         print(datos)
+else:
+    print("usage: python3 karaoke.py file.smil.")
 
 
